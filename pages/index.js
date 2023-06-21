2143b8896b5054b687301820df265ba3f0bad389
TatyanaGanya <121543437+TatyanaGanya@users.noreply.github.com>
2023-06-21 04:26:05 +0300

    х

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 // Когда пользователь нажимает на кнопку, переключаться раскрывает содержимое
 
+
+
 const myFunction = function(){
-  document.querySelector(".nav-1").classList.toggle("nav_aktiv");
+  document.querySelector(".nav-1").classList.add("nav_aktiv");
 }
 
 document.querySelector(".menu").addEventListener('click', () =>{
